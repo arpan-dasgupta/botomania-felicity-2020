1462ae0691882c87a937319870b0e19add1ede8e
YoogottamK <yoogottamk@outlook.com>
2020-02-02 12:14:20 +0530

    lint

--- board.py
+++ board.py
@@ -1,168 +1,168 @@
 class Board:
-
-	def __init__(self):
-		# init empty board
-		self.size = 8
-		self.board = [['-' for j in range(self.size)] for i in range(self.size)]
-		self.numOccupied = 0
-		self.counts = {}
-		self.isGameOver = False
-
-	def print_board(self):
-		print("\n==============================================")
-		for i in range(self.size):
-			for j in range(self.size):
-				print(self.board[i][j], end=" ")
-			print("")
-		print("==============================================\n")
-
-	def gameOver(self):
-		#returns the winning symbol or -1 in case of a draw or -2 in case of game not over
-		if self.numOccupied == self.size ** 2:
-			self.isGameOver = True
-			for i in range(self.size):
-				for j in range(self.size):
-					if self.board[i][j] in counts:
-						counts[self.board[i][j]] += 1
-					else:
-						counts[self.board[i][j]] = 1
-
-			#get list of playing symbols
-			symbols = list(counts.keys())
-
-			#total domination
-			if len(symbols) == 1:
-				return symbols[0]
-			#check winner or draw
-			else:
-				if counts[symbols[0]] > counts[symbols[1]]:
-					return symbols[0]
-				elif counts[symbols[0]] < counts[symbols[1]]:
-					return symbols[1]
-				else:
-					return -1
+    def __init__(self):
+        # init empty board
+        self.size = 8
+        self.board = [['-' for j in range(self.size)] for i in range(self.size)]
+        self.numOccupied = 0
+        self.counts = {}
+        self.isGameOver = False
 
 
-	def check_valid(self, i, j):
-		if self.board[i][j] == '-':
-			return True
-		return False
+    def print_board(self):
+        print("\n==============================================")
+        for i in range(self.size):
+            for j in range(self.size):
+                print(self.board[i][j], end=" ")
+            print("")
+        print("==============================================\n")
 
-	def update(self, i, j, c):
-		
-		# put in place
-		self.board[i][j] = c
-		self.numOccupied += 1
 
-		#update vertically up
-		k = i - 1
-		willFlip = False
-		while k >= 0 and self.board[k][j] != '-':
-			if self.board[k][j] == c:
-				willFlip = True
-				break
-			k -= 1
-		if k != -1 and willFlip:
-			for q in range(i-1, k, -1):
-				self.board[q][j] = c
+    def gameOver(self):
+        #returns the winning symbol or -1 in case of a draw or -2 in case of game not over
+        if self.numOccupied == self.size ** 2:
+            self.isGameOver = True
+            for i in range(self.size):
+                for j in range(self.size):
+                    if self.board[i][j] in self.counts:
+                        self.counts[self.board[i][j]] += 1
+                    else:
+                        self.counts[self.board[i][j]] = 1
 
-		#update vertically down
-		k = i + 1
-		willFlip = False
-		while k < self.size and self.board[k][j] != '-':
-			if self.board[k][j] == c:
-				willFlip = True
-				break
-			k += 1
-		if k != self.size and willFlip:
-			for q in range(i+1, k):
-				self.board[q][j] = c
+            #get list of playing symbols
+            symbols = list(self.counts.keys())
 
-		#update horizontally left
-		k = j - 1
-		willFlip = False
-		while k >= 0 and self.board[i][k] != '-':
-			if self.board[i][k] == c:
-				willFlip = True
-				break
-			k -= 1
-		if k != -1 and willFlip:
-			for q in range(j-1, k, -1):
-				self.board[i][q] = c
+            #total domination
+            if len(symbols) == 1:
+                return symbols[0]
+            #check winner or draw
+            else:
+                if self.counts[symbols[0]] > self.counts[symbols[1]]:
+                    return symbols[0]
+                elif self.counts[symbols[0]] < self.counts[symbols[1]]:
+                    return symbols[1]
+                else:
+                    return -1
 
-		#update horizontally right
-		k = j + 1
-		willFlip = False
-		while k < self.size and self.board[i][k] != '-':
-			if self.board[i][k] == c:
-				willFlip = True
-				break
-			k += 1
-		if k != self.size and willFlip:
-			for q in range(j+1, k):
-				self.board[i][q] = c
 
-		# update diagonally top-left
-		k = i - 1
-		l = j - 1
-		willFlip = False
-		while k >= 0 and l >= 0 and self.board[k][l] != '-':
-			if self.board[k][l] == c:
-				willFlip = True
-				break
-			k -= 1
-			l -= 1
-		if k != -1 and l != -1 and willFlip:
-			w = j - 1
-			for q in range(i-1, k, -1):
-				self.board[q][w] = c
-				w -= 1
+    def check_valid(self, i, j):
+        if self.board[i][j] == '-':
+            return True
+        return False
 
-		# update diagonally top-right
-		k = i - 1
-		l = j + 1
-		willFlip = False
-		while k >= 0 and l < self.size and self.board[k][l] != '-':
-			if self.board[k][l] == c:
-				willFlip = True
-				break
-			k -= 1
-			l += 1
-		if k != -1 and l != self.size and willFlip:
-			w = j + 1
-			for q in range(i-1, k, -1):
-				self.board[q][w] = c
-				w += 1
 
-		# update diagonally bottom-left
-		k = i + 1
-		l = j - 1
-		willFlip = False
-		while k < self.size and l >= 0 and self.board[k][l] != '-':
-			if self.board[k][l] == c:
-				willFlip = True
-				break
-			k += 1
-			l -= 1
-		if k != self.size and l != -1 and willFlip:
-			w = j - 1
-			for q in range(i+1, k):
-				self.board[q][w] = c
-				w -= 1
+    def update(self, i, j, c):
+        # put in place
+        self.board[i][j] = c
+        self.numOccupied += 1
 
-		# update diagonally bottom-right
-		k = i + 1
-		l = j + 1
-		willFlip = False
-		while k < self.size and l < self.size and self.board[k][l] != '-':
-			if self.board[k][l] == c:
-				willFlip = True
-				break
-			k += 1
-			l += 1
-		if k != self.size and l != self.size and willFlip:
-			w = j + 1
-			for q in range(i+1, k):
-				self.board[q][w] = c
-				w += 1
-				
+        #update vertically up
+        k = i - 1
+        willFlip = False
+        while k >= 0 and self.board[k][j] != '-':
+            if self.board[k][j] == c:
+                willFlip = True
+                break
+            k -= 1
+        if k != -1 and willFlip:
+            for q in range(i-1, k, -1):
+                self.board[q][j] = c
+
+        #update vertically down
+        k = i + 1
+        willFlip = False
+        while k < self.size and self.board[k][j] != '-':
+            if self.board[k][j] == c:
+                willFlip = True
+                break
+            k += 1
+        if k != self.size and willFlip:
+            for q in range(i+1, k):
+                self.board[q][j] = c
+
+        #update horizontally left
+        k = j - 1
+        willFlip = False
+        while k >= 0 and self.board[i][k] != '-':
+            if self.board[i][k] == c:
+                willFlip = True
+                break
+            k -= 1
+        if k != -1 and willFlip:
+            for q in range(j-1, k, -1):
+                self.board[i][q] = c
+
+        #update horizontally right
+        k = j + 1
+        willFlip = False
+        while k < self.size and self.board[i][k] != '-':
+            if self.board[i][k] == c:
+                willFlip = True
+                break
+            k += 1
+        if k != self.size and willFlip:
+            for q in range(j+1, k):
+                self.board[i][q] = c
+
+        # update diagonally top-left
+        k = i - 1
+        l = j - 1
+        willFlip = False
+        while k >= 0 and l >= 0 and self.board[k][l] != '-':
+            if self.board[k][l] == c:
+                willFlip = True
+                break
+            k -= 1
+            l -= 1
+        if k != -1 and l != -1 and willFlip:
+            w = j - 1
+            for q in range(i-1, k, -1):
+                self.board[q][w] = c
+                w -= 1
+
+        # update diagonally top-right
+        k = i - 1
+        l = j + 1
+        willFlip = False
+        while k >= 0 and l < self.size and self.board[k][l] != '-':
+            if self.board[k][l] == c:
+                willFlip = True
+                break
+            k -= 1
+            l += 1
+        if k != -1 and l != self.size and willFlip:
+            w = j + 1
+            for q in range(i-1, k, -1):
+                self.board[q][w] = c
+                w += 1
+
+        # update diagonally bottom-left
+        k = i + 1
+        l = j - 1
+        willFlip = False
+        while k < self.size and l >= 0 and self.board[k][l] != '-':
+            if self.board[k][l] == c:
+                willFlip = True
+                break
+            k += 1
+            l -= 1
+        if k != self.size and l != -1 and willFlip:
+            w = j - 1
+            for q in range(i+1, k):
+                self.board[q][w] = c
+                w -= 1
+
+        # update diagonally bottom-right
+        k = i + 1
+        l = j + 1
+        willFlip = False
+        while k < self.size and l < self.size and self.board[k][l] != '-':
+            if self.board[k][l] == c:
+                willFlip = True
+                break
+            k += 1
+            l += 1
+        if k != self.size and l != self.size and willFlip:
+            w = j + 1
+            for q in range(i+1, k):
+                self.board[q][w] = c
+                w += 1
--- simulator.py
+++ simulator.py
@@ -1,44 +1,42 @@
 from board import Board
 
 def gameplay(obj1, obj2):
-	#main game function
+    #main game function
 
-	game_board = Board()
-	moveFunctions = [obj1, obj2]
-	players = ['o', 'x']
-	curPlayer = 0
+    game_board = Board()
+    moveFunctions = [obj1, obj2]
+    players = ['o', 'x']
+    curPlayer = 0
 
-	#preliminary print
-	game_board.print_board()
+    #preliminary print
+    game_board.print_board()
 
-	while True:
+    while True:
 
-		winner = game_board.gameOver()
-		if game_board.isGameOver:
-			if winner == -1:
-				print("Draw")
-			else:
-				print(winner, "wins")
-			quit()
+        winner = game_board.gameOver()
+        if game_board.isGameOver:
+            if winner == -1:
+                print("Draw")
+            else:
+                print(winner, "wins")
+            quit()
 
-		else:
-			print(players[curPlayer], "to move")
-			#replace this block with bot moves
-			#=================================
-			while True:
-				i, j = input("Enter space separated row and col number: ").split(" ")
-				#add type check if needed
-				i, j = int(i), int(j)
-				if game_board.check_valid(i, j):
-					break
-				print("Invalid Move. Go again.")
-			#=================================
-			
-			game_board.update(i, j, players[curPlayer])
-			game_board.print_board()
+        else:
+            print(players[curPlayer], "to move")
+            #replace this block with bot moves
+            #=================================
+            while True:
+                i, j = input("Enter space separated row and col number: ").split(" ")
+                #add type check if needed
+                i, j = int(i), int(j)
+                if game_board.check_valid(i, j):
+                    break
+                print("Invalid Move. Go again.")
+            #=================================
 
-			curPlayer = (curPlayer + 1) % 2
+            game_board.update(i, j, players[curPlayer])
+            game_board.print_board()
+
+            curPlayer = (curPlayer + 1) % 2
 
 gameplay(1, 2)
-
-
