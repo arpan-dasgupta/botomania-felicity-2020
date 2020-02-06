6f1e018ca9ed43956fe65ddc8cd3f2b8b13c201a
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-02-07 02:31:27 +0530

    Add C/C++ support

---
+++ userscript/user_compiled.py
@@ -0,0 +1,49 @@
+import subprocess
+import requests
+import sys
+
+if __name__ == "__main__":
+    URL = "https://threads.iiit.ac.in:5000/game/get_status"
+    URL_POST = "https://threads.iiit.ac.in:5000/game/make_move"
+    player_id = 1
+
+    # sending get request and saving the response as response object
+    while(True):
+        r = requests.get(url=URL)
+        # extracting data in json format
+        data = r.json()
+
+        if data['success'] == True and data['turn'] == player_id:
+            # Write data in file
+            inp = open('input.in', 'w')
+            for i in range(0, 10):
+                for j in range(0, 10):
+                    inp.write(f"{data['state'][i*10 + j]} ")
+                inp.write('\n')
+            inp.close()
+
+            print(data)
+
+            # call user binary here
+            try:
+                subprocess.run('./a.out', timeout=1)
+                out = open('output.out', 'r')
+                r_pos = out.readline()[0]
+                c_pos = out.readline()[0]
+                out.close()
+                data = {
+                    'r_pos': int(r_pos)
+                    'c_pos': int(c_pos)
+                }
+            except subprocess.TimoutExpired:
+                print("Program ran too long")
+                data = {'r_pos': -1,
+                        'c_pos': -1}
+
+            # sending post request and saving response as response object
+            r = requests.post(url=URL_POST, data=data)
+
+            # extracting response text
+            pastebin_url = r.text
+            print(pastebin_url)
+        time.sleep(1)
