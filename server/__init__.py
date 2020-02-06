926cb6c25a689bab035f68a293813cc4d45e3bba
YoogottamK <yoogottamk@outlook.com>
2020-02-07 00:20:20 +0530

    synced with server]

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,11 @@
 from app import app
+import sys
 
-# run the app on 0.0.0.0, port 5000
+if len(sys.argv) == 1:
+    print("Please provide the port on which the app should run")
+    exit()
+
+port = sys.argv[1]
+
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port, ssl_context=("cert.pem", "key.pem"))
