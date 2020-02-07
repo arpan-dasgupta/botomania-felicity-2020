f28b1fc01c59fba478e5d78206b8766ec85cdde3
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-02-07 13:10:48 +0530

    Fix board scaling

--- app/src/Game/Square.js
+++ app/src/Game/Square.js
@@ -5,12 +5,12 @@ export default class Square extends React.Component {
     return (
       <td className="square p-0" >
         {this.props.value === 1 &&
-          <svg height="3em" width="3em">
+          <svg height="2.8em" width="2.8em">
             <circle cx="1.5em" cy="1.5em" r="1em" fill="#111"></circle>
           </svg>
         }
         {this.props.value === 2 &&
-          <svg height="3em" width="3em">
+          <svg height="2.8em" width="2.8em">
             <circle cx="1.5em" cy="1.5em" r="1em" fill="#eee"></circle>
           </svg>
         }
